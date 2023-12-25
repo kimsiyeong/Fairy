f2782b833063a178469425e72c8efa4c4ac67e5d
//G0G// <80445119+ktx06155@users.noreply.github.com>
2023-12-25 23:48:08 +0900

    Update DBHelper.java

--- app/src/main/java/com/cookandroid/fairy/DBHelper.java
+++ app/src/main/java/com/cookandroid/fairy/DBHelper.java
@@ -1,9 +1,13 @@
 package com.cookandroid.fairy;
 
+import static android.content.Context.MODE_PRIVATE;
+import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.Display;
 import android.view.View;
 
 public class DBHelper extends SQLiteOpenHelper {
@@ -16,9 +20,11 @@ public class DBHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+//        db = openOrCreateDatabase("fairyDB", null, null);
+
         db.execSQL("CREATE TABLE IF NOT EXISTS User (UID INTEGER PRIMARY KEY autoincrement," +
                 "name varchar(20) not null," +
                 "id VARCHAR(20) NOT NULL, pw VARCHAR(40) NOT NULL," +
@@ -29,6 +35,27 @@ public class DBHelper extends SQLiteOpenHelper {
         // pw 는회원가입할 때 썼던 비밀번호
         // birth 는 생년월일
         // intro 는 사용자의 한줄 소개
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Calender (UID INTEGER PRIMARY KEY," +
+                "theme varchar(10) not null," +
+                "foreign key(UID) references User(UID) on delete cascade)");
+        // theme 은 테마
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Photo (UID INTEGER," +
+                "calendarDate Text, photoPath varchar(50) not null," +
+                "updateDate Text not null," +
+                "PRIMARY KEY (UID, calendarDate)," +
+                "FOREIGN KEY(UID) references User(UID) on delete cascade)");
+        // calendarDate 는 달력에서 선택한 날짜
+        // photoPath 는 사진의 절대경로
+        // updateDate 는 최종 수정 날짜
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS Post (UID INTEGER," +
+                "calendarDate Text, content varchar(100) not null," +
+                "updateDate Text not null," +
+                "PRIMARY KEY (UID, calendarDate)," +
+                "FOREIGN KEY(UID) references User(UID) on delete cascade)");
+        // content는 게시글의 내용
 
     }
 
@@ -41,11 +68,12 @@ public class DBHelper extends SQLiteOpenHelper {
         return exists;
     }
 
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS User");
+        db.execSQL("DROP TABLE IF EXISTS Calendar");
+        db.execSQL("DROP TABLE IF EXISTS Photo");
+        db.execSQL("DROP TABLE IF EXISTS Post");
         onCreate(db);
     }
 }
